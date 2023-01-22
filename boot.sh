3d0ad060260061a5130c4ec1c5472be3b63fcb77
Joaquim Campelo da Silva Neto <joaquim.neto@ati.to.gov.br>
2023-01-22 00:44:12 -0300

    🔧 Adiciona git ignore

--- boot.sh
+++ boot.sh
@@ -10,8 +10,8 @@ export MINIO_SECRET_KEY="EXAMPLESECRET"
 export GITEA_ADMIN_USER="example"
 export DRONE_RPC_SECRET="$(echo ${HOSTNAME} | openssl dgst -md5 -hex)"
 export DRONE_USER_CREATE="username:${GITEA_ADMIN_USER},machine:false,admin:true,token:${DRONE_RPC_SECRET}"
-export DRONE_GITEA_CLIENT_ID="759e9bc1-761e-47c0-b193-a375fc70972e"
-export DRONE_GITEA_CLIENT_SECRET="mEjOubWhwXB3Dc9Q3otYrIAD-9if4TZ8wLEhtkd7Gks="
+export DRONE_GITEA_CLIENT_ID="87f0e713-16ba-4ab5-8657-cd6d9b15bf65"
+export DRONE_GITEA_CLIENT_SECRET="y-GCWFGi0BOc_fYMMzSZnAm-9bz4n3mjGz_OT5JjD-Q="
 docker-compose up -d --build
 
 echo ""
